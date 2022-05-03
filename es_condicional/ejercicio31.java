0771f24b9cf3d35e8981ae42678b303d4b84b44c
AlbertMT10 <frankcoaquirajusto4@gmail.com>
2022-05-03 17:43:52 +0000

    trabajo en clase

---
+++ es_condicional/ejercicio31.java
@@ -0,0 +1,28 @@
+package es_condicional;
+
+import java.util.Scanner;
+
+/**
+ * ejercicio31
+ */
+public class ejercicio31 {
+
+  static Scanner lt=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //declarar variables
+        int edad;
+        String mensaje;
+        //datos de entrada
+        System.out.println("ingrese la edad: ");
+        edad=lt.nextInt();
+        //proceso
+        if (edad>=18) {
+            mensaje="puede votar por que su edad es " +edad;
+        } else {
+            mensaje="no puede votar por que su edad es " +edad;
+        }
+        // datos de salida
+        System.out.println(mensaje);
+    }
+}
